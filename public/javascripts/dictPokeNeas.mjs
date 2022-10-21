a36dfef63e1e61bf8b04cb01420df76195eb9bdc
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2022-10-21 09:03:51 -0500

    Initial commit

---
+++ public/javascripts/dictPokeNeas.mjs
@@ -0,0 +1,18 @@
+var dictPokeNea = [
+    {   
+      'id':'0',
+      'Name':'Alejandro',
+      'Tall':'1.70',
+      'Skill':'Sleep',
+      'Image':'',
+      'Phrase':'Que chimba so'
+    },
+    {   
+      'id':'1',
+      'Name':'Felipe',
+      'Tall':'1.80',
+      'Skill':'Eat',
+      'Image':'',
+      'Phrase':'Pa delante es pa allá' 
+    }
+  ];
---
+++ routes/view1.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+var dictPokeNea = [
+    {   
+      'id':'0',
+      'Name':'Alejandro',
+      'Tall':'1.70',
+      'Skill':'Sleep',
+      'Image':'',
+      'Phrase':'Que chimba so'
+    },
+    {   
+      'id':'1',
+      'Name':'Felipe',
+      'Tall':'1.80',
+      'Skill':'Eat',
+      'Image':'',
+      'Phrase':'Pa delante es pa allá' 
+    }
+  ];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const number = Math.floor(Math.random() * Object.keys(dictPokeNea).length);
+  res.render('view1', { title: 'Taller 2', 'id':number, 'pokeNea':  dictPokeNea[number]})
+});
+
+module.exports = router;
